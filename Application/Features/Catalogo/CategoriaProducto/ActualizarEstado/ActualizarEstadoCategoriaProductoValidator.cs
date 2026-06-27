8278141a1513c3ef656f74ff35fc115c758b9ec8
Miguel Gonzalez <mig_24@hotmail.com>
2026-06-27 11:00:17 -0500

    feat(catalogo): crear ActualizarEstadoValidator para CategoriaProducto, UnidadMedida, ParametroSistema, Pais, Moneda, ModuloSistema

---
+++ Application/Features/Catalogo/CategoriaProducto/ActualizarEstado/ActualizarEstadoCategoriaProductoValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Application.Features.Catalogo.CategoriaProducto.ActualizarEstado.CategoriaProducto;
+
+public class ActualizarEstadoCategoriaProductoValidator : AbstractValidator<ActualizarEstadoCategoriaProductoCommand>
+{
+    public ActualizarEstadoCategoriaProductoValidator()
+    {
+        RuleFor(x => x.Id)
+            .GreaterThan(0).WithMessage("El id debe ser mayor a 0");
+    }
+}
