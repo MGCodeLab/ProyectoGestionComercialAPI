bbff5a7efc838f6db3f70b7b48cf9f4803efa2f5
Miguel Gonzalez <mig_24@hotmail.com>
2026-06-27 11:00:17 -0500

    feat(catalogo): crear ActualizarEstadoValidator para TipoImpuesto, TipoComprobante, SerieDocumento, MarcaProducto, ListaPrecio, CondicionPago

---
+++ Application/Features/Catalogo/ListaPrecio/ActualizarEstado/ActualizarEstadoListaPrecioValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Application.Features.Catalogo.ListaPrecio.ActualizarEstado.ListaPrecio;
+
+public class ActualizarEstadoListaPrecioValidator : AbstractValidator<ActualizarEstadoListaPrecioCommand>
+{
+    public ActualizarEstadoListaPrecioValidator()
+    {
+        RuleFor(x => x.Id)
+            .GreaterThan(0).WithMessage("El id debe ser mayor a 0");
+    }
+}
---
+++ Application/Features/Catalogo/MarcaProducto/ActualizarEstado/ActualizarEstadoMarcaProductoValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Application.Features.Catalogo.MarcaProducto.ActualizarEstado.MarcaProducto;
+
+public class ActualizarEstadoMarcaProductoValidator : AbstractValidator<ActualizarEstadoMarcaProductoCommand>
+{
+    public ActualizarEstadoMarcaProductoValidator()
+    {
+        RuleFor(x => x.Id)
+            .GreaterThan(0).WithMessage("El id debe ser mayor a 0");
+    }
+}
